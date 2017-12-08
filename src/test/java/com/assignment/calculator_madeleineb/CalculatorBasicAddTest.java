3f581af7c608c5965dce62eb476b88a9eff327ef
MadBar <madeleinetrollvik@gmail.com>
2017-12-08 11:06:17 +0100

    Completed Addition tests, pos, neg, zeros

--- src/test/java/com/assignment/calculator_madeleineb/CalculatorBasicAddTest.java
+++ src/test/java/com/assignment/calculator_madeleineb/CalculatorBasicAddTest.java
@@ -25,23 +25,129 @@ public class CalculatorBasicAddTest {
 		double resultFromMethod = 0;
 
 		for (int i = 0; i < 50; i++) {
-			firstRandomNr = Double
-					.valueOf(df.format(r.nextDouble()*10));
-			secondRandomNr = Double
-					.valueOf(df.format(r.nextDouble()*10));
+			firstRandomNr = Double.valueOf(
+					df.format(r.nextDouble() * 10));
+			secondRandomNr = Double.valueOf(
+					df.format(r.nextDouble() * 10));
 			result = firstRandomNr + secondRandomNr;
 			resultFromMethod = Math.round(
 					cb.add(firstRandomNr, secondRandomNr));
 
-			LOG.info("Testing the method Add with: "
-					+ firstRandomNr + " and "
-					+ secondRandomNr
-					+ ". Result from test: " + result
-					+ ", result from method: "
-					+ resultFromMethod);
+			LOG.info(
+					"Testing the method Add positive with: "
+							+ firstRandomNr + " and "
+							+ secondRandomNr
+							+ ". Result from test: "
+							+ result
+							+ ", result from method: "
+							+ resultFromMethod);
 			assertEquals(resultFromMethod, result, 1);
 		}
 
 	}
 
+	@Test
+	public void testAddNegative() {
+
+		double firstRandomNr = 0;
+		double secondRandomNr = 0;
+		double result = 0;
+		double resultFromMethod = 0;
+
+		for (int i = 0; i < 50; i++) {
+			firstRandomNr = Double.valueOf(
+					df.format(r.nextDouble() * -10));
+			secondRandomNr = Double.valueOf(
+					df.format(r.nextDouble() * -10));
+			result = firstRandomNr + secondRandomNr;
+			resultFromMethod = Math.round(
+					cb.add(firstRandomNr, secondRandomNr));
+
+			LOG.info(
+					"Testing the method Add negative with: "
+							+ firstRandomNr + " and "
+							+ secondRandomNr
+							+ ". Result from test: "
+							+ result
+							+ ", result from method: "
+							+ resultFromMethod);
+			assertEquals(resultFromMethod, result, 1);
+		}
+
+	}
+
+	@Test
+	public void testAddFirstZero() {
+
+		double firstRandomNr = 0;
+		double secondRandomNr = 0;
+		double result = 0;
+		double resultFromMethod = 0;
+
+		for (int i = 0; i < 50; i++) {
+			secondRandomNr = Double.valueOf(
+					df.format(r.nextDouble() * 10));
+			result = firstRandomNr + secondRandomNr;
+			resultFromMethod = Math.round(
+					cb.add(firstRandomNr, secondRandomNr));
+
+			LOG.info(
+					"Testing the method Add first zero with: "
+							+ firstRandomNr + " and "
+							+ secondRandomNr
+							+ ". Result from test: "
+							+ result
+							+ ", result from method: "
+							+ resultFromMethod);
+			assertEquals(resultFromMethod, result, 1);
+		}
+
+	}
+
+	@Test
+	public void testAddSecondZero() {
+
+		double firstRandomNr = 0;
+		double secondRandomNr = 0;
+		double result = 0;
+		double resultFromMethod = 0;
+
+		for (int i = 0; i < 50; i++) {
+			firstRandomNr = Double.valueOf(
+					df.format(r.nextDouble() * 10));
+
+			result = firstRandomNr + secondRandomNr;
+			resultFromMethod = Math.round(
+					cb.add(firstRandomNr, secondRandomNr));
+
+			LOG.info(
+					"Testing the method Add second zero with: "
+							+ firstRandomNr + " and "
+							+ secondRandomNr
+							+ ". Result from test: "
+							+ result
+							+ ", result from method: "
+							+ resultFromMethod);
+			assertEquals(resultFromMethod, result, 1);
+		}
+
+	}
+
+	@Test
+	public void testAddBothZeros() {
+
+		double firstRandomNr = 0;
+		double secondRandomNr = 0;
+		double result = firstRandomNr + secondRandomNr;
+		double resultFromMethod = Math.round(
+				cb.add(firstRandomNr, secondRandomNr));
+
+		LOG.info("Testing the method Add both zeros with: "
+				+ firstRandomNr + " and " + secondRandomNr
+				+ ". Result from test: " + result
+				+ ", result from method: "
+				+ resultFromMethod);
+		assertEquals(resultFromMethod, result, 0);
+
+	}
 }
