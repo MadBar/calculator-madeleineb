8632570aaeae797d814bc3111588aa00c919a612
MadBar <madeleinetrollvik@gmail.com>
2017-12-11 17:03:27 +0100

    All operations are connected to the buttons, and resetbutton is operational.

--- src/main/java/com/assignment/calculator_madeleineb/MainFrame.java
+++ src/main/java/com/assignment/calculator_madeleineb/MainFrame.java
@@ -11,15 +11,29 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import java.awt.Font;
+import java.awt.Insets;
+
+import javax.swing.JSeparator;
 
 public class MainFrame {
+	
+	CalculatorBasic cb = new CalculatorBasic();
+	CalculatorAdvanced ca = new CalculatorAdvanced();
 
 	//Why do they get the property Final? Auto.
+	//Components Attributes
 	private JFrame frmMadbarCalculator = new JFrame();
+	JSeparator separator = new JSeparator();
+	
 	private final JLabel lblPickNr = new JLabel("Fill the two fields below with two numbers you wish to operate");
-	private final JLabel lblOperation = new JLabel("Chose operation to perform");
+	private final JLabel lblOperation = new JLabel("Choose basic operation to perform to get result");
+	private final JLabel lblNrToRaise = new JLabel("...Or insert a number below to raise");
+	private final JLabel lblResult = new JLabel("Result: ");
+	
 	private final JTextField textField1Nr = new JTextField();
 	private final JTextField textField2Nr = new JTextField();
+	private final JTextField textFieldResult = new JTextField();
+	private final JTextField textFieldNrToRaise = new JTextField();
 	
 	private final JButton btnPlus = new JButton("+");
 	private final JButton btnSubt = new JButton("-");
@@ -33,9 +47,12 @@ public class MainFrame {
 	private final JButton btnPow6 = new JButton("x6");
 	private final JButton btnPow7 = new JButton("x7");
 	
-	private final JLabel lblResult = new JLabel("Result: ");
-	private final JTextField textFieldResult = new JTextField();
-	
+	private final JButton btnReset = new JButton("Reset");
+
+	//Created Attributes
+	private double firstNr = 0.0;
+	private double secondNr = 0.0;	
+	private double result = 0.0;
 
 	/**
 	 * Launch the application.
@@ -58,6 +75,10 @@ public class MainFrame {
 	 * Create the application.
 	 */
 	public MainFrame() {
+		textFieldNrToRaise.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		textFieldNrToRaise.setHorizontalAlignment(SwingConstants.RIGHT);
+		textFieldNrToRaise.setBounds(83, 166, 188, 28);
+		textFieldNrToRaise.setColumns(10);
 		initialize();
 		addComponents();
 		addActionListeners();
@@ -71,51 +92,73 @@ public class MainFrame {
 
 		// Properties of Frame
 		frmMadbarCalculator.setTitle("MadBar Calculator");
-		frmMadbarCalculator.setBounds(100, 100, 366, 304);
+		frmMadbarCalculator.setBounds(100, 100, 365, 385);
 		frmMadbarCalculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmMadbarCalculator.getContentPane().setLayout(null);
 		frmMadbarCalculator.setVisible(true);
 		
 		// Properties of Text Fields
 		textFieldResult.setEditable(false);
-		textFieldResult.setBounds(60, 218, 263, 20);
+		textFieldResult.setBounds(69, 264, 254, 28);
 		textFieldResult.setColumns(10);
 		textField1Nr.setToolTipText("insert number");
 		textField1Nr.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField1Nr.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		textField1Nr.setBounds(20, 38, 141, 40);
+		textField1Nr.setBounds(20, 27, 141, 28);
 		textField1Nr.setColumns(10);
 		textField2Nr.setToolTipText("insert number");
 		textField2Nr.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField2Nr.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		textField2Nr.setColumns(10);
-		textField2Nr.setBounds(171, 38, 147, 40);
+		textField2Nr.setBounds(171, 27, 147, 28);
+		textFieldResult.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		// Properties of Labels
+		lblPickNr.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPickNr.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblPickNr.setBounds(20, 11, 303, 16);
+		lblOperation.setHorizontalAlignment(SwingConstants.CENTER);
+		lblOperation.setBounds(20, 57, 298, 14);
+		lblResult.setBounds(20, 264, 47, 28);
+		lblNrToRaise.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNrToRaise.setBounds(20, 142, 298, 14);
 
 		// Properties of basic buttons
 		btnPlus.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnPlus.setBounds(258, 113, 65, 40);
-		btnSubt.setBounds(171, 112, 65, 40);
-		btnDivi.setBounds(96, 112, 65, 40);
-		btnMult.setBounds(20, 112, 65, 40);
+		btnPlus.setBounds(258, 75, 65, 40);
+		btnSubt.setBounds(171, 74, 65, 40);
+		btnDivi.setBounds(96, 74, 65, 40);
+		btnMult.setBounds(20, 74, 65, 40);
 
 		// Properties of advanced buttons
-		btnPow2.setFont(new Font("Tahoma", Font.PLAIN, 7));
-		btnPow2.setBounds(21, 163, 40, 40);
-		btnPow3.setFont(new Font("Tahoma", Font.PLAIN, 7));
-		btnPow3.setBounds(71, 163, 40, 40);
+		btnPow2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnPow2.setBounds(20, 213, 40, 40);
+		btnPow3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnPow3.setMargin(new Insets(1,1,1,1));
+//		setMargin(new Insets(1,1,1,1));
+		btnPow3.setBounds(69, 213, 40, 40);
 		btnPow4.setFont(new Font("Tahoma", Font.PLAIN, 6));
-		btnPow4.setBounds(121, 164, 40, 40);
+		btnPow4.setBounds(119, 213, 40, 40);
 		btnPow5.setFont(new Font("Tahoma", Font.PLAIN, 6));
-		btnPow5.setBounds(181, 163, 40, 40);
+		btnPow5.setBounds(181, 213, 40, 40);
 		btnPow6.setFont(new Font("Tahoma", Font.PLAIN, 6));
-		btnPow6.setBounds(233, 164, 40, 40);
+		btnPow6.setBounds(231, 213, 40, 40);
 		btnPow7.setFont(new Font("Tahoma", Font.PLAIN, 6));
-		btnPow7.setBounds(283, 164, 40, 40);
+		btnPow7.setBounds(283, 213, 40, 40);
+		
+		btnReset.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnReset.setBounds(20, 303, 303, 32);
+		
+		//Other
+		separator.setBounds(20, 126, 303, 16);
 	}
 
 	public void addComponents() {
 		
 		frmMadbarCalculator.getContentPane().add(textField1Nr);
+		frmMadbarCalculator.getContentPane().add(textField2Nr);
+		frmMadbarCalculator.getContentPane().add(textFieldResult);
+		frmMadbarCalculator.getContentPane().add(textFieldNrToRaise);
 		
 		frmMadbarCalculator.getContentPane().add(btnPlus);
 		frmMadbarCalculator.getContentPane().add(btnSubt);
@@ -129,20 +172,14 @@ public class MainFrame {
 		frmMadbarCalculator.getContentPane().add(btnPow6);
 		frmMadbarCalculator.getContentPane().add(btnPow7);
 		
-		
-		frmMadbarCalculator.getContentPane().add(textField2Nr);
-		lblPickNr.setVerticalAlignment(SwingConstants.TOP);
-		lblPickNr.setBounds(20, 11, 298, 16);
+		frmMadbarCalculator.getContentPane().add(btnReset);
 		
 		frmMadbarCalculator.getContentPane().add(lblPickNr);
-		lblOperation.setBounds(104, 89, 141, 14);
-		
 		frmMadbarCalculator.getContentPane().add(lblOperation);
-		lblResult.setBounds(20, 214, 40, 28);
-		
 		frmMadbarCalculator.getContentPane().add(lblResult);
+		frmMadbarCalculator.getContentPane().add(lblNrToRaise);
 		
-		frmMadbarCalculator.getContentPane().add(textFieldResult);
+		frmMadbarCalculator.getContentPane().add(separator);
 	}
 
 	public void addActionListeners() {
@@ -152,52 +189,91 @@ public class MainFrame {
 				// System.out.println("Clickeddd!");
 //				frmMadbarCalculator.getContentPane().setBackground(Color.red);
 				
-		
-				/*Hämta siffra från text field. 
-				 * spara i en variabel? 
-				 * Rensa textfield
-				 * 
-				 * 
-				 * */
+				firstNr = Double.parseDouble(textField1Nr.getText());
+				secondNr = Double.parseDouble(textField2Nr.getText());
+				result = cb.add(firstNr, secondNr);
+				textFieldResult.setText(String.valueOf(result));
 			}
 		});
 		btnSubt.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				firstNr = Double.parseDouble(textField1Nr.getText());
+				secondNr = Double.parseDouble(textField2Nr.getText());
+				result = cb.subtract(firstNr, secondNr);
+				textFieldResult.setText(String.valueOf(result));
 			}
 		});
 		btnDivi.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				firstNr = Double.parseDouble(textField1Nr.getText());
+				secondNr = Double.parseDouble(textField2Nr.getText());
+				result = cb.divide(firstNr, secondNr);
+				textFieldResult.setText(String.valueOf(result));
 			}
 		});
 		btnMult.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				firstNr = Double.parseDouble(textField1Nr.getText());
+				secondNr = Double.parseDouble(textField2Nr.getText());
+				result = cb.multiply(firstNr, secondNr);
+				textFieldResult.setText(String.valueOf(result));
+				
 			}
 		});
 		
 		//Advanced
 		btnPow2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				firstNr = Double.parseDouble(textFieldNrToRaise.getText());
+				result = ca.pow2(firstNr);
+				textFieldResult.setText(String.valueOf(result));
 			}
 		});
 		btnPow3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				firstNr = Double.parseDouble(textFieldNrToRaise.getText());
+				result = ca.pow3(firstNr);
+				textFieldResult.setText(String.valueOf(result));
 			}
 		});
 		btnPow4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				firstNr = Double.parseDouble(textFieldNrToRaise.getText());
+				result = ca.pow4(firstNr);
+				textFieldResult.setText(String.valueOf(result));
 			}
 		});
 		btnPow5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				firstNr = Double.parseDouble(textFieldNrToRaise.getText());
+				result = ca.pow5(firstNr);
+				textFieldResult.setText(String.valueOf(result));
 			}
 		});
 		btnPow6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				firstNr = Double.parseDouble(textFieldNrToRaise.getText());
+				result = ca.pow6(firstNr);
+				textFieldResult.setText(String.valueOf(result));
 			}
 		});
 		btnPow7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				firstNr = Double.parseDouble(textFieldNrToRaise.getText());
+				result = ca.pow7(firstNr);
+				textFieldResult.setText(String.valueOf(result));
+			}
+		});
+		
+		btnReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField1Nr.setText("");
+				textField2Nr.setText("");
+				textFieldResult.setText("");
+				textFieldNrToRaise.setText("");
 			}
 		});
 	}
