34f96e49841cdf8b1d2a198f66159eb710ffc212
MadBar <madeleinetrollvik@gmail.com>
2017-12-13 16:18:23 +0100

    Renamed MainFrame2 to MainFrame. Going to use this. MainFrameOpt is unfinished. Not for use. Not a part of assignment. GUI finished

--- src/main/java/com/assignment/calculator_madeleineb/Main.java
+++ src/main/java/com/assignment/calculator_madeleineb/Main.java
@@ -5,8 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		CalculatorBasic calB = new CalculatorBasic();
 
-		System.out.println(calB.divide(4, 0));
+//		System.out.println(calB.divide(4, 0));
+		System.out.println(calB.add(4, 4));
 
+		
 	}
 
 }
